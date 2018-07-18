d25688ea6e477e385bb1699cd1340ccff921ed08
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-07-18 21:48:23 +0700

    Restructure project

--- mobile/plugins/axios.js
+++
@@ -1,9 +0,0 @@
-import * as axios from 'axios'
-
-let options = {}
-// The server-side needs a full url to works
-if (process.server) {
-  options.baseURL = `http://${process.env.HOST || 'localhost'}:${process.env.PORT || 8010}`
-}
-
-export default axios.create(options)
