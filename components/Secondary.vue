d303852ca59a0e69b18cab70e5679391452703ef
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-07-19 15:44:19 +0700

    redesign editor

--- components/Secondary.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="secondary">foo</div>
-</template>
--- pages/studio/editor.vue
+++ pages/studio/editor.vue
@@ -4,38 +4,82 @@
     <div class="content-flex">
       <div class="studio">
         <div class="studio-inner">
-          <Container group-name="studio" :get-child-payload="targetElement" @drop="onDrop('target', $event)">            
-            <Draggable v-for="item in target" :key="item.id">
-              <component :is="item.component"/>
+          <Container
+            group-name="studio"
+            :get-child-payload="targetElement"
+            :drag-handle-selector="`.toolbar_handle`"
+            drag-class="smooth-dnd-drag"
+            drop-class="smooth-dnd-drop"
+            @drop="onDrop('target', $event)"
+            @drag="onDrag('target', $event)"
+          >
+            <Draggable v-for="(item, key) in target" :key="key">
+              <div class="component-item" @click="setEditable(key)">
+                <div class="component-toolbar">
+                  <span class="component-toolbar_item toolbar_handle">
+                    <i class="ion-arrow-move"></i>
+                  </span>
+                  <span class="component-toolbar_item toolbar_delete" @click="removeComponent(key)">
+                    <i class="ion-trash-b"></i>
+                  </span>
+                </div>
+                <component :is="item.component"/>
+              </div>
             </Draggable>
           </Container>
         </div>
       </div>
-      <div class="secondary">
-        <Container :behaviour="'copy'" group-name="studio" :get-child-payload="sourceElement">
-          <Draggable v-for="item in source" :key="item.id">
-            <div class="smooth-dnd-item">
-              {{item.data}}
-            </div>
-          </Draggable>
-        </Container>
+      <div class="properties">
+        <div role="tablist">
+          <b-card no-body>
+            <b-card-header header-tag="header" role="tab" v-b-toggle.accordion1>
+              Layouts
+            </b-card-header>
+            <b-collapse id="accordion1" visible accordion="my-accordion" role="tabpanel">
+              <b-card-body>
+                <Container
+                  :behaviour="'copy'"
+                  group-name="studio"
+                  :get-child-payload="sourceElement"
+                  drag-class="smooth-dnd-drag"
+                  drop-class="smooth-dnd-drop"
+                >
+                  <Draggable v-for="item in source" :key="item.id">
+                    <div class="smooth-dnd-item">
+                      {{item.data}}
+                    </div>
+                  </Draggable>
+                </Container>
+              </b-card-body>
+            </b-collapse>
+          </b-card>
+          <b-card no-body>
+            <b-card-header header-tag="header" role="tab" v-b-toggle.accordion2>
+              Components
+            </b-card-header>
+            <b-collapse id="accordion2" accordion="my-accordion" role="tabpanel">
+              <b-card-body>
+                <p class="card-text">
+                  Beatae et eos minus nulla sint inventore, repellat quidem, reiciendis hic adipisci quod laudantium odio voluptate modi a. Debitis dignissimos nostrum voluptatem.
+                </p>
+              </b-card-body>
+            </b-collapse>
+          </b-card>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import panzoom from 'panzoom'
 import { Container, Draggable } from 'vue-smooth-dnd'
 import Navbar from '~/components/Navbar.vue'
-import Secondary from '~/components/Secondary.vue'
 
 export default {
   components: {
     Container,
     Draggable,
-    Navbar,
-    Secondary
+    Navbar
   },
   data () {
     return {
@@ -55,19 +99,20 @@ export default {
       target: []
     }
   },
-  mounted () {
-    if (process.browser) {
-      panzoom(document.querySelector('.studio-inner'))
-    }
-  },
   methods: {
-    getComponent (path) {
-      return ({
-        component: import(`${path}`)
-      })
+    removeComponent (index) {
+      this.target.splice(index, 1)
     },
     onDrop (collection, dropResult) {
       this[collection] = this.applyDrag(this[collection], dropResult)
+    },
+    setEditable (index) {
+      const component = document.querySelectorAll('.component-item')
+
+      component.forEach(item => item.classList.remove('component-item_active'))
+      component[index].classList.add('component-item_active')
+
+      document.querySelectorAll('.component-item [data-oct-text]').forEach(item => item.setAttribute('contenteditable', true))
     },
     sourceElement (index) {
       return this.source[index]
