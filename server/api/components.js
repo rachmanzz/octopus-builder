54d579fc94404331c2b00245b3524af891a007c6
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-07-25 13:25:23 +0700

    Add attribute from git status at list components

--- server/api/components.js
+++ server/api/components.js
@@ -28,42 +28,27 @@ const getComponents = () => {
     })
   })
 }
-
-router.get('/component/map', (req, res) => {
-  getComponents().then(allFiles => {
-    const listComponents = allFiles.map(item => {
-      return `{
-        name: ${JSON.stringify(item.name)},
-        icon: 'far fa-image',
-        component: () => import(${JSON.stringify(item.path.replace('./', '~/'))})
-      }`
-    }).join(',\n')
-
-    const string = `
-      /**
-       * THIS FILE IS AUTO-GENERATE WHEN
-       * BUTTON REFRESH ON BUILDER CLICKED
-       */
-      const listComponents = [${listComponents}]
-      export default listComponents
-    `
-
-    fs.writeFileSync('./lib/studioImporter.js', prettier.format(string, {
-      semi: false,
-      singleQuote: true,
-      parser: 'babylon'
-    }))
-
-    res.json({
-      success: true,
-      data: []
-    })
-  })
-})
-
 router.get('/component', (req, res) => {
   getComponents().then(allFiles => {
-    res.json(allFiles)
+    git('./core').status((err, gitFilter) => {
+      if (err) {
+        throw new Error(err)
+      }
+
+      const gitFilterParam = ['not_added', 'conflicted', 'created', 'deleted', 'modified', 'renamed']
+
+      gitFilterParam.map(item => {
+        const filteredFile = gitFilter[item]
+
+        allFiles.map(file => {
+          if (filteredFile.indexOf(file.path.replace('./core/', '')) > -1) {
+            file['status'] = item
+          }
+        })
+      })
+
+      res.json(allFiles)
+    })
   })
 })
 
@@ -152,6 +137,45 @@ router.post('/component/publish', (req, res) => {
         branch: branch
       })
     })
+})
+
+router.get('/component/status', (req, res) => {
+  git('./core').status((err, status) => {
+    if (err) throw new Error(err)
+    res.json(status)
+  })
+})
+
+router.get('/component/map', (req, res) => {
+  getComponents().then(allFiles => {
+    const listComponents = allFiles.map(item => {
+      return `{
+        name: ${JSON.stringify(item.name)},
+        icon: 'far fa-image',
+        component: () => import(${JSON.stringify(item.path.replace('./', '~/'))})
+      }`
+    }).join(',\n')
+
+    const string = `
+      /**
+       * THIS FILE IS AUTO-GENERATE WHEN
+       * BUTTON REFRESH ON BUILDER CLICKED
+       */
+      const listComponents = [${listComponents}]
+      export default listComponents
+    `
+
+    fs.writeFileSync('./lib/studioImporter.js', prettier.format(string, {
+      semi: false,
+      singleQuote: true,
+      parser: 'babylon'
+    }))
+
+    res.json({
+      success: true,
+      data: []
+    })
+  })
 })
 
 export default router
