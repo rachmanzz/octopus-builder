c71d67fe9fa0d68b76ee532503a905ee8154f09d
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-07-27 16:00:18 +0700

    latest push from improve dnd

--- pages/studio/editor.vue
+++ pages/studio/editor.vue
@@ -5,43 +5,39 @@
       <div class="studio">
         <div class="studio-inner"></div>
       </div>
-      <div class="properties">
-        <div role="tablist">
+      <div class="properties" role="tablist">
+        <!-- Layouts -->
+        <b-card class="card-default" no-body>
+          <b-card-header header-tag="header" role="tab" v-b-toggle.layouts>
+            Layouts
+          </b-card-header>
+          <b-collapse id="layouts" accordion="editor-collapse" role="tabpanel">
+            <b-card-body>
+              no layout
+            </b-card-body>
+          </b-collapse>
+        </b-card>
 
-          <!-- Layouts -->
-          <b-card class="card-default" no-body>
-            <b-card-header header-tag="header" role="tab" v-b-toggle.layouts>
-              Layouts
-            </b-card-header>
-            <b-collapse id="layouts" accordion="editor-collapse" role="tabpanel">
-              <b-card-body>
-                no layout
-              </b-card-body>
-            </b-collapse>
-          </b-card>
-
-          <!-- Components -->
-          <b-card class="card-default" no-body>
-            <b-card-header header-tag="header" role="tab" v-b-toggle.components>
-              Components
-            </b-card-header>
-            <b-collapse id="components" visible accordion="editor-collapse" role="tabpanel">
-              <b-card-body class="builder-element">
-                <div class="builder-item" v-for="(item, key) in source" :key="key">
-                  <div class="builder-icon" :class="item.icon" :title="item.name"></div>
-                  <div class="builder-name">{{ item.name }}</div>
-                  <div class="builder-component">
-                    <component :is="item.component"></component>
-                  </div>
+        <!-- Components -->
+        <b-card class="card-default" no-body>
+          <b-card-header header-tag="header" role="tab" v-b-toggle.components>
+            Components
+          </b-card-header>
+          <b-collapse id="components" visible accordion="editor-collapse" role="tabpanel">
+            <b-card-body class="studio-element">
+              <div class="studio-source" v-for="(item, key) in source" :key="key">
+                <div class="studio-icon" :class="item.icon" :title="item.name"></div>
+                <div class="studio-name">{{ item.name }}</div>
+                <div class="studio-component">
+                  <component :is="item.component"></component>
                 </div>
-              </b-card-body>
-            </b-collapse>
-          </b-card>
+              </div>
+            </b-card-body>
+          </b-collapse>
+        </b-card>
 
-          <!-- Properties -->
-          <Properties />
-
-        </div>
+        <!-- Properties -->
+        <Properties />
       </div>
     </div>
   </div>
@@ -50,8 +46,8 @@
 <script>
 import axios from 'axios'
 import Builder from '~/lib/studioBuilder.js'
-import importer from '~/lib/studioImporter.js'
-import extractor from '~/lib/studioExtractor.js'
+import Source from '~/lib/studioSource.js'
+import Extractor from '~/lib/studioExtractor.js'
 import Navbar from '~/components/global/Navbar.vue'
 import Properties from '~/components/studio/Properties.vue'
 
@@ -72,14 +68,15 @@ export default {
     }
   },
   mounted () {
+    this.source = Source
+
     if (process.browser) {
       new Builder() // eslint-disable-line
     }
-    this.source = importer
   },
   methods: {
     save () {
-      extractor.generateMap().then(mapping => {
+      Extractor.generateMap().then(mapping => {
         console.log(JSON.stringify(mapping))
         // axios.post('/api/render', mapping).then(result => {
         //   this.$snotify.success('Generate page success')
